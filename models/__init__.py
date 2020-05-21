43ca678c562d1e72ccd489a96da222d4edb97895
Ainsley Rutterford <ar16478@bristol.ac.uk>
2020-05-21 12:00:07 +0100

    Add learning rate argument

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,16 @@
 import models.models as M
 import models.losses as L
-from keras.optimizers import Adam
+from keras.optimizers import Adam, SGD
 
-def compile(arch='unet2D', loss='binary_cross', pretrained_weights=None, size=256):
+def compile(arch='unet2D', loss='binary_cross', pretrained_weights=None, size=256, lr=0.00005):
     model = getattr(M, arch)(size=size)
 
     if loss == 'focal':
-        model.compile(optimizer=Adam(lr = 1e-4), loss=L.binary_focal_loss(), metrics=["accuracy"])
+        model.compile(optimizer=Adam(lr=lr), loss=L.binary_focal_loss(), metrics=["accuracy"])
     elif loss == 'dice':
-        model.compile(optimizer=Adam(lr = 1e-4), loss=L.dice_coef_loss, metrics=["accuracy"])
+        model.compile(optimizer=Adam(lr=lr), loss=L.dice_coef_loss, metrics=["accuracy"])
     else:
-        model.compile(optimizer=Adam(lr = 1e-4), loss="binary_crossentropy", metrics=["accuracy"])
+        model.compile(optimizer=Adam(lr=lr), loss="binary_crossentropy", metrics=["accuracy"])
 
     if pretrained_weights:
         model.load_weights(pretrained_weights)
