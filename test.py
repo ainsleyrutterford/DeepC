b5c1b95077fdc6c1a52394bf381524db6f0a25b8
Ainsley Rutterford <ar16478@bristol.ac.uk>
2020-05-28 22:13:25 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,9 +3,10 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='unet2D', help='Architecture [unet2D | unet3D | segnet2D]')
 parser.add_argument('--size', type=int, default=256, help='Size to reshape the images to when training')
+parser.add_argument('--tests', type=int, default=56, help='The number of tests to carry out')
 parser.add_argument('--weights', type=str, default='isambard-20.hdf5', help='Weights file to load')
-parser.add_argument('--loss', type=str, default='binary_cross', help='Loss function [binary_cross | focal | dice]')
 parser.add_argument('--verbose', action='store_true', help='Show TensorFlow startup messages and warnings')
+parser.add_argument('--ablated', action='store_true', help='Use ablated architecture')
 parser.add_argument('--dir', type=str, default='data/', help='Data directory')
 args = parser.parse_args()
 
@@ -20,14 +21,13 @@ import data
 from datetime import datetime
 from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping
 
-model = models.compile(args.model, args.loss, size=args.size, pretrained_weights=args.weights)
+model = models.compile(arch=args.model, pretrained_weights=args.weights, size=args.size, abl=args.ablated)
 
-num_tests = 66
 if args.model == 'unet3D':
-    test_gen = data.test_generator_3D(args.dir + "/test/image", num_image=num_tests)
-    results = model.predict_generator(test_gen, num_tests, verbose=1)
+    test_gen = data.test_generator_3D(f"{args.dir}/test/image", num_image=args.tests)
+    results = model.predict_generator(test_gen, args.tests, verbose=1)
     data.save_result("test", results)
 else:
-    test_gen = data.test_generator(args.dir + "/test/image", num_image=num_tests, target_size=(args.size, args.size))
-    results = model.predict_generator(test_gen, num_tests, verbose=1)
+    test_gen = data.test_generator(f"{args.dir}/test/image", num_image=args.tests, target_size=(args.size, args.size))
+    results = model.predict_generator(test_gen, args.tests, verbose=1)
     data.save_result("test", results)
