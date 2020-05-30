59d376c19770809e2e209496a9680b2dbe65b2dd
Ainsley Rutterford <ar16478@bristol.ac.uk>
2020-05-30 18:48:16 +0100

    Comment losses.py

--- models/losses.py
+++ models/losses.py
@@ -2,13 +2,23 @@ import tensorflow as tf
 from keras import backend as K
 
 def binary_focal_loss(gamma=2., alpha=.25):
+    """
+        An implementation of the focal loss function introduced in:
+
+        https://arxiv.org/abs/1708.02002
+
+        This Keras implementation is taken from:
+
+        https://github.com/mkocabas/focal-loss-keras
+
+        Args:
+            gamma: (float) the "focusing" parameter
+            alpha: (float) the weighting factor
+        Returns:
+            loss: (arr) a tensor containing the loss values.
+        """
 
     def binary_focal_loss_fixed(y_true, y_pred):
-        """
-        :param y_true: A tensor of the same shape as `y_pred`
-        :param y_pred:  A tensor resulting from a sigmoid
-        :return: Output tensor.
-        """
         pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
         pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
 
@@ -21,10 +31,3 @@ def binary_focal_loss(gamma=2., alpha=.25):
                -K.sum((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
 
     return binary_focal_loss_fixed
-
-def dice_coef_loss(y_true, y_pred):
-    smooth = 1
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    return 1 - ((2.0 * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth))
