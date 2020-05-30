be9cef20d9120d3729b916e4099433e1d3e7191c
Ainsley Rutterford <ar16478@bristol.ac.uk>
2020-05-30 14:50:58 +0100

    Finish commenting train.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ parser.add_argument('--tests', type=int, default=56, help='The number of tests t
 parser.add_argument('--weights', type=str, default='isambard-20.hdf5', help='Weights file to load')
 parser.add_argument('--verbose', action='store_true', help='Show TensorFlow startup messages and warnings')
 parser.add_argument('--ablated', action='store_true', help='Use ablated architecture')
-parser.add_argument('--dir', type=str, default='data/', help='Data directory')
+parser.add_argument('--dir', type=str, default='data/test/image', help='Data directory')
 args = parser.parse_args()
 
 if not args.verbose:
@@ -24,10 +24,10 @@ from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping
 model = models.compile(arch=args.model, pretrained_weights=args.weights, size=args.size, abl=args.ablated)
 
 if args.model == 'unet3D':
-    test_gen = data.test_generator_3D(f"{args.dir}/test/image", num_image=args.tests)
+    test_gen = data.test_generator_3D(f"{args.dir}", num_image=args.tests)
     results = model.predict_generator(test_gen, args.tests, verbose=1)
     data.save_result("test", results)
 else:
-    test_gen = data.test_generator(f"{args.dir}/test/image", num_image=args.tests, target_size=(args.size, args.size))
+    test_gen = data.test_generator(f"{args.dir}", num_image=args.tests, target_size=(args.size, args.size))
     results = model.predict_generator(test_gen, args.tests, verbose=1)
     data.save_result("test", results)
